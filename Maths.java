7b6d25cb4144ca086fb5b6bfedc550a5cdd4c23c
Velan <velan@gmail.com>
2024-03-12 12:15:32 +0530

    Division Operation Done

--- Maths.java
+++ Maths.java
@@ -3,6 +3,9 @@ class Maths{
   //subtraction
   //multiply
   //division
+  Static double division(int a ,int b){
+     return a/b;
+  } 
   //modulus
   public static void main(String args[]){
   }
