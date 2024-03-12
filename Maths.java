aed0102ad58ae02869b08a757c2596e7af12d2ec
Aravind <aravindpandiyarajan2004@gmail.com>
2024-03-12 12:12:57 +0530

    Add done by aravind

--- Maths.java
+++ Maths.java
@@ -2,6 +2,11 @@ class Maths{
   //addition
   //subtraction
   //multiply
+  static int mul(int a,int b){
+	  int c = a*b;
+	return c;
+   }
+	  
   //division
   //modulus
   public static void main(String args[]){
