eb2bc1bff77bc97cf83ca0bb6cdf27c38948af01
Aravind <aravindpandiyarajan2004@gmail.com>
2024-03-12 12:14:59 +0530

    mul done by aravind

--- Maths.java
+++ Maths.java
@@ -2,6 +2,10 @@ class Maths{
   //addition
   //subtraction
   //multiply
+  static int mul(int a,int b){
+	  int c = a*b;
+	 return c;
+}
   //division
   //modulus
   public static void main(String args[]){
